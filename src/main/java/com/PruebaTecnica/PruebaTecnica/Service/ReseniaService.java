766901eceeade40eca295ae8790fc39db6609d4c
dpa3006 <dpa3006@gmail.com>
2024-02-27 12:23:00 -0500

    controller

--- src/main/java/com/PruebaTecnica/PruebaTecnica/Service/ReseniaService.java
+++ src/main/java/com/PruebaTecnica/PruebaTecnica/Service/ReseniaService.java
@@ -1,4 +1,28 @@
 package com.PruebaTecnica.PruebaTecnica.Service;
+import com.PruebaTecnica.PruebaTecnica.Entity.Resenia;
+import com.PruebaTecnica.PruebaTecnica.Repository.ReseniaRepository;
+import org.hibernate.sql.Delete;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ReseniaService {
+    ReseniaRepository reseniaRepository;
+
+    public List<Resenia> getResenia(){
+        return reseniaRepository.findAll();
+    }
+    public Optional<Resenia> getResenia(Integer id){
+        return reseniaRepository.findById(id);
+    }
+    public void saveOrUpdate(Resenia resenia){
+        reseniaRepository.save(resenia);
+    }
+
+    public void delete (Integer id){
+        reseniaRepository.deleteById(id);
+    }
 }
+
