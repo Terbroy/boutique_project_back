23895f33b1bcaf9c05c963bee8959c4f4e1be949
omar gomez <omargo91@gmail.com>
2024-02-27 21:04:19 -0500

    arreglos en categorias

--- src/main/java/com/boutiqueProject/Repository/CategoriasRepository.java
+++ src/main/java/com/boutiqueProject/Repository/CategoriasRepository.java
@@ -1,4 +1,9 @@
 package com.boutiqueProject.Repository;
 
-public class CategoriasRepository {
+import com.boutiqueProject.Entity.Categorias;
+import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CategoriasRepository extends JpaRepository<Categorias, String> {
 }
--- src/main/java/com/boutiqueProject/Service/CategoriaService.java
+++ src/main/java/com/boutiqueProject/Service/CategoriaService.java
@@ -1,4 +1,36 @@
 package com.boutiqueProject.Service;
 
+import com.boutiqueProject.Entity.Categorias;
+import com.boutiqueProject.Entity.Productos;
+import com.boutiqueProject.Repository.CategoriasRepository;
+import com.boutiqueProject.Repository.ProductoRepository;
+import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.management.loading.PrivateClassLoader;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CategoriaService {
+    @Autowired
+    CategoriasRepository categoriasRepository;
+
+
+    public List<Productos> getcategorias(){
+        return categoriasRepository.findAll(); // get
+    }
+    public Optional<Categorias> getProducto(String id){
+        return categoriasRepository.findById(id);
+    }
+
+    public void saveOrUpdate(Categorias categorias){
+        categoriasRepository.save(categorias); // post --> crear && put||patch --> actualizar
+    }
+
+    public void delete(String id){
+        categoriasRepository.deleteById(id);// delete -->eliminar
+    }
+
 }
